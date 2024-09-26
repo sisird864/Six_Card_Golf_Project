9023e6a64825336ef3433ba047a0511f6b097cf1
sisird864 <137139127+sisird864@users.noreply.github.com>
2024-09-25 17:21:40 -0700

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -2,8 +2,8 @@ import socket
 import threading
 import sys
 
-players = {}
-games = {}
+players = []
+games = []
 
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -13,6 +13,29 @@ ip_address = socket.gethostbyname(hostname)
 
 sock1.bind((ip_address, 40100))
 
+def register_func(command, addr):
+    list1 = command.split(" ")
+    for i in players:
+        if list1[1] == i[0]:
+            print("FAILURE")
+            return
+    tup1 = (list[1], list[2], list[3], list[4])
+    players.append(tup1)
+
+def deregister_func(command, addr):
+    list1 = command.split(" ")
+    for i in range(len(players)):
+        if players[i] == list1[1]:
+            players.pop(i)
+            return
+    print("FAILURE")
+    return
+
+
 while True:
-    data, addr = sock1.recvfrom(1024)
-    print(data, " ::: ", addr)
+    command, addr = sock1.recvfrom(1024)
+    if command.startswith("register"): register_func(command, addr)
+    elif command == "query players": print(players)
+    elif command == "query games": print(games)
+    elif command.startswith("de-register"): deregister_func(command, addr)
+    else: print("Invalid Command!")
