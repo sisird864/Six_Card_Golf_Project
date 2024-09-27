48b7456f5170652e90410dfd48a9bf97482d4251
sisird864 <137139127+sisird864@users.noreply.github.com>
2024-09-27 12:48:47 -0700

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -17,22 +17,26 @@ def register_func(command, addr):
     list1 = command.split(" ")
     for i in players:
         if list1[1] == i[0]:
-            print("FAILURE")
+            sock1.sendto("FAILURE".encode('utf-8'), (addr[0], addr[1]))
             return
     tup1 = (list1[1], list1[2], list1[3], list1[4])
     players.append(tup1)
+    sock1.sendto("SUCCESS".encode('utf-8'), (addr[0], addr[1]))
+    return
+
 
 def deregister_func(command, addr):
     list1 = command.split(" ")
     for i in range(len(players)):
         if players[i][0] == list1[1]:
             players.pop(i)
+            sock1.sendto("SUCCESS".encode('utf-8'), (addr[0], addr[1]))
             return
-    print("FAILURE")
+    sock1.sendto("FAILURE".encode('utf-8'), (addr[0], addr[1]))
     return
 
 def query_players(addr):
-    players_str = '\n'.join(players)
+    players_str = '\n'.join([' '.join(player) for player in players])
     sock1.sendto(players_str.encode('utf-8'), (addr[0], addr[1]))
     return
 
