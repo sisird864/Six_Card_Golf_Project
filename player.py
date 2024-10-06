cb1906d6e76344cc36fb9d1e5cb987066f3d2df1
sisird864 <137139127+sisird864@users.noreply.github.com>
2024-10-06 12:07:47 -0700

    Add files via upload

--- player.py
+++ player.py
@@ -2,22 +2,23 @@ import socket
 import threading
 import sys
 # Checks if the player started the program with the correct parameters.
-if len(sys.argv) != 3:
+if len(sys.argv) != 5:
     print("invalid command!")
     sys.exit()
 
 # The parameters that need to be provided are the tracker's ip address and the port number that the tracker uses.
 tracker_ip = sys.argv[1]
 tracker_port = int(sys.argv[2])
-
+t_port = int(sys.argv[3])
+p_port = int(sys.argv[4])
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
 sock_tracker = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creates a UDP socket for communicating with the tracker
-sock_tracker.bind((ip_address, 40200)) # Uses port 40200 for the socket
+sock_tracker.bind((ip_address, t_port)) # Assigns port for the socket
 
 sock_player = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creates a UDP socket for communicating with other players
-sock_player.bind((ip_address, 40300)) # Uses port 40300 for the socket
+sock_player.bind((ip_address, p_port)) # Assigns port for the socket
 
 # Main loop for user to send the messages to the tracker.
 # Receives and outputs the messages from the tracker.
