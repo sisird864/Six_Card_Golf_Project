c2c8c6aacb1a2c3d1fed0c0cd037af0c0cb9bec4
sisird864 <137139127+sisird864@users.noreply.github.com>
2024-10-11 21:53:54 -0700

    Add files via upload

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import sys
 import random
+from threading import Event
 
 # Checks if the player started the program with the correct parameters.
 if len(sys.argv) != 5:
@@ -26,92 +27,108 @@ sock_player.bind((ip_address, p_port))  # Assigns port for the socket
 # Store player information after starting the game
 players_info = []
 am_I_dealer = False
-
-# Create a lock to synchronize access to the input prompt
-lock = threading.Lock()
-
 def receive_messages():
-    global players_info, cards, cards_facing_up, discard_pile
+    global print_ready
     while True:
         message, addr = sock_player.recvfrom(1024)
         message = message.decode('utf-8')
+        #print(f"Received from {addr}: {message}")
 
-        # Acquire lock before processing the message
-        with lock:
-            # Check if the message contains player information (broadcasted by the dealer)
-            if message.startswith("PLAYER_INFO"):
-                cards_facing_up = set()
-                cards = [[], []]
-                players_info = message.splitlines()[1:]  # Store player information from the broadcast
-                print("\nNew Game Started!\nPlayers in the game:")
-                for player in players_info:
-                    print(player)
+        # Check if the message contains player information (broadcasted by the dealer)
+        if message.startswith("PLAYER_INFO"):
+            global players_info
+            global cards
+            global cards_facing_up
+            cards_facing_up = set()
+            cards = [[], []]
+            players_info = message.splitlines()[1:]  # Store player information from the broadcast
+            print("\nNew Game Started!\nPlayers in the game:")
+            for player in players_info:
+                print(player)
+            print_ready.set()
+        elif message.startswith("New Card:"):
+            new_card = message.splitlines()[1]
+            if len(cards[0]) == 3:
+                cards[1].append(new_card)
+            else: cards[0].append(new_card)
+            if len(cards[1]) == 3:
+                while True:
+                    c = cards[random.randint(0,1)][random.randint(0,2)]
+                    if c in cards_facing_up:
+                        continue
+                    cards_facing_up.add(c)
+                    if len(cards_facing_up) == 2: break
+                row1 = ""
+                for card in cards[0]:
+                    if card not in cards_facing_up: row1 += "*** "
+                    else:
+                        if len(card) == 2: row1 += f" {card} "
+                        else: row1 += f"{card} "
+                row2 = ""
+                for card in cards[1]:
+                    if card not in cards_facing_up:
+                        row2 += "*** "
+                    else:
+                        if len(card) == 2:
+                            row2 += f" {card} "
+                        else:
+                            row2 += f"{card} "
+                if am_I_dealer: print("\n")
+                print(row1)
+                print(row2)
+                print_ready.set()
+        elif message == "query discard pile":
+            discard_pile_top = discard_pile[len(discard_pile)-1]
+            sock_player.sendto(discard_pile_top.encode('utf-8'), (addr[0], addr[1]))
+        else:
+            print(message)
+            print_ready.set()
 
-            elif message.startswith("New Card:"):
-                new_card = message.splitlines()[1]
-                if len(cards[0]) == 3:
-                    cards[1].append(new_card)
-                else:
-                    cards[0].append(new_card)
-
-                if len(cards[1]) == 3:
-                    while True:
-                        c = cards[random.randint(0, 1)][random.randint(0, 2)]
-                        if c in cards_facing_up:
-                            continue
-                        cards_facing_up.add(c)
-                        if len(cards_facing_up) == 2:
-                            break
-                    row1 = " ".join([f" {card} " if card in cards_facing_up else "***" for card in cards[0]])
-                    row2 = " ".join([f" {card} " if card in cards_facing_up else "***" for card in cards[1]])
-
-                    if am_I_dealer:
-                        print("\n")
-                    print(row1)
-                    print(row2)
-
-            elif message == "query discard pile":
-                discard_pile_top = discard_pile[-1]
-                sock_player.sendto(discard_pile_top.encode('utf-8'), addr)
-
-            else:
-                print(message)
 
 # Start a thread for receiving messages from other players
 threading.Thread(target=receive_messages, daemon=True).start()
 
 # Main loop for sending messages to the tracker or other players.
 while True:
-    with lock:
-        command = input("Enter your command here: ")
+    print_ready.wait()
+    print_ready.clear()
+    command = input("Enter your command here: ")
 
     # Check if it's a command for the tracker
     if command.startswith("register") or command == "query players" or command == "query games" or command.startswith("de-register") or command.startswith("start") or command.startswith("end"):
         sock_tracker.sendto(command.encode('utf-8'), (tracker_ip, tracker_port))
         message, addr = sock_tracker.recvfrom(1024)
         message = message.decode('utf-8')
-        with lock:
-            print(message)
+        print(message)
 
+        # If the game starts successfully and you're the dealer
         if command.startswith("start"):
             players_info = message.splitlines()[2:]  # Assume player info starts from the second line
+            """print("Players in the game:")
+            for player in players_info:
+                print(player)"""
             am_I_dealer = True
+            global deck
+            global cards
+            global discard_pile
             discard_pile = []
+            global cards_facing_up
             cards_facing_up = set()
-            cards = [[], []]
-            ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+            cards = [[],[]]
+            ranks = ['1','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
             suits = ['H', 'D', 'C', 'S']
+            # Create the deck by combining each rank with each suit
             deck = [f'{rank}{suit}' for rank in ranks for suit in suits]
+            # Shuffles the deck
             random.shuffle(deck)
 
             # Broadcast player information to all other players
             player_list_message = "PLAYER_INFO\n" + "\n".join(players_info)
-            for player in players_info[1:]:
+            for player in players_info[1:]:  # Skip the first entry (dealer)
                 player_info = player.split()
                 player_ip = player_info[1]
                 player_port = int(player_info[2])
                 sock_player.sendto(player_list_message.encode('utf-8'), (player_ip, player_port))
-
             for i in range(6):
                 for player in players_info:
                     player_info = player.split()
@@ -119,7 +136,6 @@ while True:
                     player_port = int(player_info[2])
                     given_card = deck.pop()
                     sock_player.sendto(f"New Card:\n{given_card}".encode('utf-8'), (player_ip, player_port))
-
             discard_pile.append(deck.pop())
 
     elif command == "query discard pile":
@@ -128,7 +144,7 @@ while True:
         player_ip = player_info[1]
         player_port = int(player_info[2])
         sock_player.sendto(command.encode('utf-8'), (player_ip, player_port))
-
+    
     else:
         # If the game has started, allow interaction with other players
         for player in players_info:
