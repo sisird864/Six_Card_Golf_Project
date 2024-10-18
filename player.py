7de68d6fcc2efa0ce3464abb36785e3324665a03
sisird864 <137139127+sisird864@users.noreply.github.com>
2024-10-17 18:48:02 -0700

    Add files via upload

--- player.py
+++ player.py
@@ -54,7 +54,7 @@ def receive_steal():
         if message.startswith("Stolen Card"):
             card_from_steal = message.splitlines()[1]
             print("Card stolen: ",card_from_steal)
-            #got_card.set()
+            got_card.set()
         elif message.startswith("Steal"):
             indexes = message.splitlines()[1]
             card_to_give = cards[int(indexes[0])][int(indexes[1])]
@@ -182,8 +182,8 @@ def receive_messages():
                         global card_from_steal
                         card_from_steal = ""
                         sock_steal.sendto(f"Steal\n{steal_position}\n{my_name}".encode('utf-8'), (player_ip_s, player_port_s))
-                        #got_card.wait()
-                        #got_card.clear()
+                        got_card.wait()
+                        got_card.clear()
                         my_card = card_from_steal
                         print("My Card: ",my_card)
                         from_steal = True
